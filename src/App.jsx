39c8e75e9ba2236347800d4dd4e152f996e39353
Senga S <sengasingh0611@gmail.com>
2025-06-27 18:48:24 -0500

    new start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1 className="text-3xl underline">Welcome to my 3D Portfolio!</h1>
+            </div>
+        </main>
+
+    )
+}
+export default App
