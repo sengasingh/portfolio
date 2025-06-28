45aed58deac25491795e05caf57d685423c06bea
Senga S <sengasingh0611@gmail.com>
2025-06-27 19:54:30 -0500

    updates to hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Hero from "./sections/Hero.jsx";
+
 const App = () => {
     return (
-        <main>
-            <div>
-                <h1 className="text-3xl underline">Welcome to my 3D Portfolio!</h1>
-            </div>
-        </main>
+        <>
+            <Hero />
+        </>
 
     )
 }
